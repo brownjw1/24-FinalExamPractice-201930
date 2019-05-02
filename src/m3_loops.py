cc1b40d0a8b98c4ba2b3f8eab4027583b86a509c
brownjw1 <brownjw1@rose-hulman.edu>
2019-05-02 10:21:42 -0400

    DUNZO

--- src/m3_loops.py
+++ src/m3_loops.py
@@ -6,8 +6,8 @@ This problem provides practice at:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Jared Brown.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
 # Students:
@@ -46,7 +46,7 @@ def main():
 def run_test_practice_problem3():
     """ Tests the   practice_problem3  function. """
     ###########################################################################
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  practice_problem3  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests beyond those we wrote.
     #
@@ -110,6 +110,12 @@ def run_test_practice_problem3():
              st.SimpleTestCase(practice_problem3,
                                [0, 1, 1.414213562373],
                                [286602]),
+             st.SimpleTestCase(practice_problem3,
+                               [1,2,0],
+                               [1,2]),
+             st.SimpleTestCase(practice_problem3,
+                               [3,4,-1],
+                               [3,5,6,7]),
              ]
     # 14th test:
     big_list = []
@@ -217,6 +223,15 @@ def practice_problem3(start, n, threshold):
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   < 15 minutes.
     ###########################################################################
+    list=[]
+    k=start
+    while True:
+        if(len(list)==n):
+            break
+        if(math.sin(k)+math.cos(k)>threshold):
+                list+=[k]
+        k+=1
+    return list
 
 
 # -----------------------------------------------------------------------------
